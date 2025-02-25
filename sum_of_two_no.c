927d450e259357be0f8d066f417b3ef130e06740
Bisal Ranjan Swain <bisalranjankanha@gmail.com>
2025-02-25 20:48:17 +0530

    Create sum_of_two_no.c

---
+++ sum_of_two_no.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf("enter two number:");
+    scanf("%d%d",&a,&b);
+    int c= a+b;
+    printf("\nsum of %d and %d is %d",a,b,c);
+    return 0;
+}
